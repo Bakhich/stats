aaff4f84df26486a30caf3e644b556d9b9bd479b
Bakhtiyor Aminzoda <hilolon@yandex.ru>
2021-04-06 16:15:55 +0500

    Начальная версия

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Bakhich/stats
+
+go 1.16
+
+require github.com/Bakhich/bank v0.0.0-20210406110544-c7c825cece5d // indirect
---
+++ pkg/stats/stats.go
@@ -0,0 +1,30 @@
+package stats
+
+import (
+	"github.com/Bakhich/bank1/v2/pkg/types"
+)
+
+//Avg расчитовает среднюю сумму платежа
+func Avg(payments []types.Payment) types.Money {
+	countPayments := types.Money(len(payments))
+	sumPaymenys := types.Money(0)
+	for _, payment := range payments {
+
+		moneyPayments := payment.Amount
+		sumPaymenys += moneyPayments
+	}
+	return sumPaymenys / countPayments
+}
+
+// TotalInCategory находит 	сумму покупок в определённой категории.
+func TotalInCategory(payments []types.Payment, category types.Category) types.Money {
+	sumPaymenys := types.Money(0)
+	for _, payment := range payments {
+		if payment.Category != category {
+			continue
+		}
+		moneyPayments := payment.Amount
+		sumPaymenys += moneyPayments
+	}
+	return sumPaymenys
+}
